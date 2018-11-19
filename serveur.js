3d24735a86ebec2e56ecc1cfcb5bdd4fef2eb68a
khoriasGZ <gomezkevin@hotmail.fr>
2018-11-19 12:43:39 +0100

    Les modules avec node

--- serveur.js
+++ serveur.js
@@ -1,10 +1,15 @@
 var http = require('http');
+var test = require('./test');
+
 var EventEmitter = require('events').EventEmitter;
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Salut tout le monde !');
 });
+
+test.direBonjour();
+test.direByeBye();
 
 var jeu = new EventEmitter();
 
